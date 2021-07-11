97822570a7d6587c093ef7df08872d6f6381d4ef
Deejay Geroso <deejaygeroso@gmail.com>
2021-07-11 22:56:03 +0800

    Added user schema IUser type

--- src/mongoose/schemas/User.ts
+++ src/mongoose/schemas/User.ts
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+import { IUser } from '../../interfaces'
 import mongoose from '../config/mongoose'
 
 const Schema = mongoose.Schema
-const User = new Schema({
-  email: String,
-  hashedPassword: String,
-  type: String,
+
+const User = new Schema<IUser>({
+  email: { type: String, required: true },
+  hashedPassword: { type: String, required: true },
+  type: { type: String, required: true },
 }, {
   collection: 'users',
   timestamps: true
