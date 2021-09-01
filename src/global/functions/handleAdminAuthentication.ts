75d519e6cf3d822a0bdfee0bb7944c4829852bcd
Deejay Geroso <deejaygeroso@gmail.com>
2021-09-01 13:23:52 +0800

    Removed unused import

--- src/global/functions/handleAdminAuthentication.ts
+++ src/global/functions/handleAdminAuthentication.ts
@@ -1,7 +1,7 @@
-import { statusCodes, userTypes } from '../constants'
 import { AuthenticationError } from 'apollo-server-express'
 import { IUser } from '../../interfaces'
 import getStatusMessage from './getStatusMessage'
+import { userTypes } from '../constants'
 
 const handleAdminAuthentication = (root: undefined, args: undefined, context: { user: IUser }): void => {
   if (context.user.type !== userTypes.admin) {
